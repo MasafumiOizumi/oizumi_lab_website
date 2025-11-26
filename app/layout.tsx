b7dd2534aff18e358e84955569f5ed3ca38c421d
MasafumiOizumi <c-oizumi@g.ecc.u-tokyo.ac.jp>
2025-11-26 22:06:35 +0900

    feat: add Netlify Identity widget script to layout head

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </head>
       <body>
         {children}
       </body>
